67957a002046b11e0da685b077f30c1451a96796
Midnight-Maverick <adhav2006@gmail.com>
2026-04-10 16:37:27 +0530

    endpoint receives logs

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from log-ingestion!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class logentry(BaseModel):
+    service_name: str
+    level: str
+    message: str
+    timestamp: str
+
+@app.post("/log")
+async def create_log(entry: logentry):
+    print(f"Received log entry: {entry.service_name}, level: {entry.level}, message: {entry.message}, timestamp: {entry.timestamp}")
+    return{"status" : "received"}
 
 
-if __name__ == "__main__":
-    main()
