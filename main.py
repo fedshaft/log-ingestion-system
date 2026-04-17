c5c8644c6211c9790a7094a4a352ffd985572a62
Midnight-Maverick <adhav2006@gmail.com>
2026-04-17 17:18:09 +0530

    added enum for better data validation

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import redis
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+class Loglevel(str, Enum):
+    INFO = "INFO"
+    ERROR = "ERROR"
+    WARNING = 'WARNING'
+
 class logentry(BaseModel):
     service_name: str
-    level: str
+    level: Loglevel
     message: str
     timestamp: str
 
